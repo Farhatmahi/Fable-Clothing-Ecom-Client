cb43d842ada8940457f8ad81343c1ab55a876a71
Farhat Mahi <farhatmahi007@gmail.com>
2022-12-01 00:02:48 +0600

    Home partially done

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='flex flex-col justify-center items-center my-20'>
+            <img src="https://i.ibb.co/1Zks0DC/Logo-1.png" className='w-48 lg:w-fit' alt="" />
+            <h1 className='text-[#3C3737] text-md text-center lg:text-lg mt-8 mb-16'>Moscow clothing brand that doesn't limit itself to the framework of any concepts</h1>
+            <img src="https://i.ibb.co/x5zGyNk/banner.png" className='' alt="" />
+        </div>
+    );
+};
+
+export default Banner;
